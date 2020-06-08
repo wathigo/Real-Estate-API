3ef0390a9de5d486b518e6bfc92c7c5499d3a84f
Simon Wathigo <wathigosimon@gmail.com>
2020-06-08 19:59:34 +0300

    Update configs to autoload 'lib' directory

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,12 +1,13 @@
 class JsonWebToken
+    SECRET = Rails.application.secrets.secret_key_base
     class << self
         def encode(payload, exp = 24.hours.from_now)
             payload[:exp] = exp.to_i
-            JWT.encode(payload, Rails.application.secrets.secret_key_base)
+            JWT.encode(payload, SECRET)
         end 
 
         def decode(token)
-            body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+            body = JWT.decode(token, SECRET)[0]
             HashWithIndifferentAccess.new body
         rescue
             nil
