05cc591ffa1f89a464f40c4f94bcf6d311778bc5
Simon Wathigo <wathigosimon@gmail.com>
2020-06-09 07:52:28 +0300

    Finish user authentication

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'jwt'
 
+gem 'simple_command'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_164313) do
+ActiveRecord::Schema.define(version: 2020_06_08_184502) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
