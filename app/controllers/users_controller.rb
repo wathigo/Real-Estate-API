3f4947814e8b189e80dfb853784191eb2475f09b
Simon Wathigo <wathigosimon@gmail.com>
2020-06-16 09:05:49 +0300

    Update flow of logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_request, only: :create
   def create
     user = User.create(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
     if user.valid?
+      auth_token = AuthenticateUser.new(user.email, user.password).call 
       render json: { auth_token: auth_token.result }
     else
       render json: { error: user.errors }, status: :unauthorized
