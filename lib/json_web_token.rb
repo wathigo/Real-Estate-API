88f573ca6153540f8a4d418e996d4a6ad5b1683d
Simon Wathigo <wathigosimon@gmail.com>
2020-06-29 13:32:32 +0300

    Remove the use of deprecated 'should' rspec

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,7 +3,7 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, SECRET) 
+      JWT.encode(payload, SECRET)
     end
 
     def decode(token)
