f826d54b446e183b700af99ac530bce0de6fec99
Simon Wathigo <wathigosimon@gmail.com>
2020-06-29 13:47:48 +0300

    Handle 'dependent: :destroy' scenarios

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  it { should belong_to(:category) }
+  it { should have_many(:favourites).dependent(:destroy) }
+  it { should have_one(:geo_location).dependent(:destroy) }
 
+  it { should belong_to(:category) }
   it { should validate_presence_of(:address) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:description) }
